469c062e847da506fe9da8945b0739c3c21596f2
Alfarqun <87592560+Alfarqun@users.noreply.github.com>
2022-02-27 20:54:49 +0700

    update referal.js

--- plugins/referal.js
+++ plugins/referal.js
@@ -13,7 +13,7 @@ const xp_bonus = {
 let handler = async (m, { conn, usedPrefix, text }) => {
   let users = global.db.data.users
   if (text) {
- //   if ('ref_count' in users[m.sender]) throw 'Tidak bisa menggunakan kode referal!'
+   if ('ref_count' in users[m.sender]) throw 'Tidak bisa menggunakan kode referal!'
     let link_creator = (Object.entries(users).find(([, { ref_code }]) => ref_code === text.trim()) || [])[0]
     if (!link_creator) throw 'Kode referal tidak valid'
     let count = users[link_creator].ref_count++
